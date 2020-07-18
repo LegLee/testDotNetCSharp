a4f880baa3d3c36f4a97d834faa0e56b20606871
LegLee <torleek@mail.ru>
2020-07-18 21:20:18 +0300

    Initial commit

---
+++ testDotNetCSharp/Calculate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace testDotNetCSharp
+{
+    public class Calculate
+    {
+        public Calculate()
+        {
+        }
+
+        public int summ(int a, int b)
+        {
+            int c;
+            c = a + b;
+            return c;
+        }
+        public int sub(int a, int b)
+        {
+            int c;
+            c = a - b;
+            return c;
+        }
+        public int mul(int a, int b)
+        {
+            int c;
+            c = a * b;
+            return c;
+        }
+        public float div(int a, int b)
+        {
+            float c=0;
+            if (b != 0)
+            {
+                c = (float)a / b;
+            }
+            else
+            {
+                Console.WriteLine("На ноль делить нельзя!\n");
+            }
+            return c;
+        }
+    }
+}
---
+++ testDotNetCSharp/MainClass.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace testDotNetCSharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a, b;
+            int number;
+            int result;
+            float resultD;
+            Calculate prog = new Calculate();
+            while(true)
+            {
+                Console.WriteLine("Введите первое число ");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите второе число ");
+                b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Выберите операцию: \n0)Выход \n1) Сумма \n2) Вчитание \n3) Умножение \n4) Деление");
+                number = Convert.ToInt32(Console.ReadLine());
+                if(number==1)
+                {
+                    result=prog.summ(a, b);
+                    Console.WriteLine($"Result: {result}");
+                }
+                else if (number == 2)
+                {
+                    result = prog.sub(a, b);
+                    Console.WriteLine($"Result: {result}");
+                }
+                else if (number == 3)
+                {
+                    result = prog.mul(a, b);
+                    Console.WriteLine($"Result: {result}");
+                }
+                else if(number == 4)
+                {
+                    resultD = prog.div(a, b);
+                    Console.WriteLine($"Result: {resultD}");
+                }
+                else if(number == 0)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
